798c02f0328bc98fe6788578f61954a2a4838f61
Elandro <elandro.caniela@gmail.com>
2020-11-06 19:21:13 -0300

    First Controller create

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Cours";
+            ViewData["Creator"] = "Elandro W. Caniela";
 
             return View();
         }
